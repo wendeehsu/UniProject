61bee8ab897f438317d9eea3dd29243dc32dc1bf
Melhor <b05705046@ntu.edu.tw>
2019-12-09 21:21:02 +0800

    arrange pickle files

--- ControlArduino.py
+++ ControlArduino.py
@@ -2,8 +2,8 @@ import serial
 import time
 
 prev = 0
-arduinoData = serial.Serial('/dev/cu.usbmodem141201',9600)
-movement = [0,180,0]
+arduinoData = serial.Serial('/dev/cu.usbmodem143401',9600)
+movement = [0,60,0]
 for i in movement:
 	time.sleep(1)
 	print("signal = ", i, " ", 3*i/float(180))
